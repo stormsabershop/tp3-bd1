bc3111481db5e788f1169eda5e4319d367592c42
Rafael Carvalho Matias <2001889@cegeplimoilou.ca>
2024-04-16 21:53:59 -0400

    tables ajoutées

--- tp3.sql
+++ tp3.sql
@@ -1,27 +1,15 @@
 CREATE DATABASE TP3jeromeOuelletRafaelMatias;
+USE TP3jeromeOuelletRafaelMatias;
 
-CREATE TABLE article
+CREATE TABLE clientelle 
 (
-art_code			VARCHAR(4) 		NOT NULL,
-art_nom				VARCHAR(20) 	NOT NULL,
-art_prix			REAL(9,2) 		NULL,
-art_qte 			INT(8) 			NULL,
+cli_num				INT(4) NOT NULL,
+cli_nom				varchar(15) NULL,
+cli_prenom			varchar(15) NULL,
+cli_solde    		REAL(9,2) NULL,
+cli_ville			varchar(20) NULL,
 
-CONSTRAINT 			pk_art_code		PRIMARY KEY (art_code),
-CONSTRAINT			uk_art_nom		UNIQUE (art_nom)
-);
-
-CREATE TABLE lignecmd
-(
-lig_num				INT(2)			NOT NULL,
-lig_com_num			INT(4)			NOT NULL,
-lig_art_code		VARCHAR(4)		NOT NULL,
-lig_qte				INT(3)          NULL,
-lig_art_prix		REAL(9,2)       NULL,
-
-CONSTRAINT			pk_lig_com_num  PRIMARY KEY (lig_num, lig_com_num),
-CONSTRAINT 			fk_lig_com_num  FOREIGN KEY (lig_com_num) REFERENCES commande (com_num),
-CONSTRAINT			fk_lig_art_code FOREIGN KEY (lig_art_code) REFERENCES article (art_code)
+CONSTRAINT 			pk_cli_num PRIMARY KEY (cli_num)
 );
 
 CREATE TABLE commande 
@@ -36,16 +24,36 @@ CONSTRAINT 			pk_com_num PRIMARY KEY (com_num),
 CONSTRAINT          fk_com_cli_num foreign key (com_cli_num) REFERENCES clientelle (cli_num)
 );
 
-CREATE TABLE clientelle 
+CREATE TABLE article
 (
-cli_num				INT(4) NOT NULL,
-cli_nom				varchar(15) NULL,
-cli_prenom			varchar(15) NULL,
-cli_solde    		REAL(9,2) NULL,
-cli_ville			varchar(20) NULL,
+art_code			VARCHAR(4) 		NOT NULL,
+art_nom				VARCHAR(20) 	NOT NULL,
+art_prix			REAL(9,2) 		NULL,
+art_qte 			INT(8) 			NULL,
 
-CONSTRAINT 			pk_cli_num PRIMARY KEY (cli_num)
+CONSTRAINT 			pk_art_code		PRIMARY KEY (art_code),
+CONSTRAINT			uk_art_nom		UNIQUE (art_nom)
 );
+
+
+CREATE TABLE lignecmd
+(
+lig_num				INT(2)			NOT NULL,
+lig_com_num			INT(4)			NOT NULL,
+lig_art_code		VARCHAR(4)		NOT NULL,
+lig_qte				INT(3)          NULL,
+lig_art_prix		REAL(9,2)       NULL,
+
+CONSTRAINT			pk_lig_com_num  PRIMARY KEY (lig_num, lig_com_num),
+CONSTRAINT 			fk_lig_com_num  FOREIGN KEY (lig_com_num) REFERENCES commande (com_num),
+CONSTRAINT			fk_lig_art_code FOREIGN KEY (lig_art_code) REFERENCES article (art_code)
+);
+
+
+
+
+
+
  
 
 
